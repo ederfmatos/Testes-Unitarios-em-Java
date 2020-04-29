7d1fe94ccd54818e5d0e7ebdfbba8bf0fd728a63
ederfmatos <ederfmatos@gmail.com>
2020-04-28 22:55:17 -0300

    Suites de tesets

---
+++ src/test/java/com/ederfmatos/testesunitarios/suites/ExecutionSuite.java
@@ -0,0 +1,15 @@
+package com.ederfmatos.testesunitarios.suites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import com.ederfmatos.testesunitarios.servicos.CalculadoraTest;
+import com.ederfmatos.testesunitarios.servicos.CalculoValorLocacaoTest;
+import com.ederfmatos.testesunitarios.servicos.LocacaoServiceTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CalculadoraTest.class, CalculoValorLocacaoTest.class, LocacaoServiceTest.class })
+public class ExecutionSuite {
+
+}
